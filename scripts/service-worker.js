f43f2d97bbfbbb4e93ae83215da35c64df7b3702
Sameer Vohra <vohra.sam@gmail.com>
2024-06-10 15:23:34 -0400

    make side panel toggle when clicking toolbar button

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,1 +1,8 @@
+// Required for content scripts to access storage.
 chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' });
+
+
+// Side panel toggle button
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
